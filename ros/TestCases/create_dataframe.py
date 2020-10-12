23581b479ff3030bab153d6930a39f8c724c9529
PedroACaldeira <caldeira.a.pedro@gmail.com>
2020-10-12 18:00:05 +0100

    [GSPN-EXE-ROS] No places one token test created.

--- ros/TestCases/create_dataframe.py
+++ ros/TestCases/create_dataframe.py
@@ -41,7 +41,7 @@ plt.legend()
 # Display a figure.
 plt.show()
 '''
-
+'''
 # Combination of both Test
 x1 = [7,14,21,28,35,42,49,56,63,70,77,84,91,98,105]
 y1 = [16.4,18.1,21.4,23.2,24,26.2,28,28.2,29.7,32.3,33.4,40.2,41.5,42.4,47.7]
@@ -57,6 +57,26 @@ plt.xlabel('action servers')
 plt.ylabel('percentage (%)')
 # Set a title of the current axes.
 plt.title('% of CPU for an increasing number of action servers')
+# show a legend on the plot
+plt.legend()
+# Display a figure.
+plt.show()
+'''
+# NoGazeboPlacesOneToken Test
+x1 = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+y1 = [15.6,16,16.4,16.7,17.3,17.6,18.5,18.6,18.6,18.8,18.6,18.8,19,19.6,20.2]
+# plotting the line 1 points
+plt.plot(x1, y1, label = "CPU %")
+# line 2 points
+x2 = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+y2 = [25.6,26.3,26.4,26.9,27.3,27.8,28.2,28.5,28.8,29.1,29.5,29.7,30.1,30.2,30.4]
+# plotting the line 2 points
+plt.plot(x2, y2, label = "Virtual memory %")
+plt.xlabel('places')
+# Set the y axis label of the current axis.
+plt.ylabel('percentage (%)')
+# Set a title of the current axes.
+plt.title('% of CPU and virtual memory for an increasing number of places with one token')
 # show a legend on the plot
 plt.legend()
 # Display a figure.
